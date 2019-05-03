7e069b54af36ad549c3cba769a5d5be0b51b4736
Princy <princypco@gmail.com>
2019-05-02 21:43:48 -0600

    update

--- src/server/MainWindow.java
+++ src/server/MainWindow.java
@@ -43,16 +43,16 @@ public class MainWindow extends JFrame
 		mainpane.add(lblNoteAboutClients);
 
 		btnCloseServer = new JButton("CLOSE THE SERVER");
-		btnCloseServer.setBounds(151, 229, 161, 59);
+		btnCloseServer.setBounds(146, 212, 161, 59);
 		mainpane.add(btnCloseServer);
 		
 		displayBoard = new JTextArea((new Timestamp(System.currentTimeMillis())).toString()+" Server is now running...");
 		displayBoard.setLineWrap(true);
 		displayBoard.setEditable(false);
-		displayBoard.setBounds(10, 76, 414, 142);
+		displayBoard.setBounds(10, 76, 414, 115);
 		JScrollPane scroll = new JScrollPane (displayBoard, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, 
 				JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-		scroll.setBounds(10, 76, 414, 142);
+		scroll.setBounds(10, 76, 414, 115);
 		mainpane.add(scroll);
 		
 		this.setTitle("Server");
